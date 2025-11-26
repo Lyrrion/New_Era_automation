de94efc621b903067fe072ecff51c1412a0856c5
Sujit Gade <SujitGade13@G24B17SaumyaAcc3outlook.onmicrosoft.com>
2025-11-26 13:21:49 +0530

    extra issue

--- Env/Dev/main.tf
+++ Env/Dev/main.tf
@@ -1,12 +1,12 @@
 module "resource_group" {
-  source          = "../Modules/Azurerm_resource_group"
+  source          = "../../Modules/Azurerm_resource_group"
   resource_groups = var.resource_groups
 }
 
 
 module "storage" {
   depends_on = [ module.resource_group ]
-  source           = "../Modules/Azurerm_storage_account"
+  source           = "../../Modules/Azurerm_storage_account"
   storage_accounts = var.storage_accounts
 
 }
