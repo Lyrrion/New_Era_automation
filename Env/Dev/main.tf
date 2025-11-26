d48ac158d0b2489dbcf19437353605a9ea244577
Sujit Gade <SujitGade13@G24B17SaumyaAcc3outlook.onmicrosoft.com>
2025-11-26 13:25:35 +0530

    extra removed

--- Env/Dev/main.tf
+++ Env/Dev/main.tf
@@ -11,21 +11,5 @@ module "storage" {
 
 }
 
-# module "vnet" {
-#   depends_on = [ module.resource_group,module.storage ]
-#   source        = "../Modules/Azurerm_virtual_network"
-#   Virtual_networks  = var.Virtual_networks
-# }
 
-# module "subnets" {
-#   depends_on = [ module.vnet ]
-#   source        = "../Modules/Azurerm_subnet"
-#   subnets  = var.subnets
-  
-# }
-
-# module "nsg" {
-#   depends_on = [ module.resource_group ]
-#   source        = "../Modules/Azurerm_network_security_group"
-#   nsgs  = var.nsgs
-# }
+##new removed 
